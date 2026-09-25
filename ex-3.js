b5cc28c25b0a61f87b2dfd962f08943f914c0133
Mile <milegame2@gmail.com>
2026-09-25 21:29:27 +0700

    Feat:Answer_Ex3

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,21 @@
 // Start coding here
+let add = (a,b) =>
+{
+    return add = a + b;
+}
+let subtrack = (a,b) =>
+{
+    return subtrack = a- b;
+}
+let multiply = (a,b) =>
+{
+    return multiply = a * b;
+}
+let divide = (a,b) =>
+{
+    return divide = a/b;
+}
+console.log(add(1,3));
+console.log(subtrack(1,4));
+console.log(multiply(1,5));
+console.log(divide(1,3));
