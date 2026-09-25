401affa798d0e759a84e3f66720510bda3107812
Mile <milegame2@gmail.com>
2026-09-25 18:52:52 +0700

    feat:Anser_Ex1

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,21 @@
 // Start coding here
+function add(a,b)
+{
+    return add = a + b;
+}
+function subtrack(a,b)
+{
+    return subtrack = a- b;
+}
+function multiply(a,b)
+{
+    return multiply = a * b;
+}
+function divide(a,b)
+{
+    return divide = a/b;
+}
+console.log(add(1,3));
+console.log(subtrack(1,4));
+console.log(multiply(1,5));
+console.log(divide(1,3));
