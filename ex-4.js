dff319af340b19003c024c20c8bfeb198e425784
Mile <milegame2@gmail.com>
2026-09-25 21:50:21 +0700

    Feat:Answer_ex4

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,23 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+    add: function(a,b)
+{
+    return a + b;
+},
+    subtrack: function(a,b)
+{
+    return a- b;
+},
+    multiply: function(a,b)
+{
+    return a * b;
+},
+    divide: function(a,b)
+{
+    return a/b;
+}
+};
+console.log(calculator.add(1,3));
+console.log(calculator.subtrack(1,4));
+console.log(calculator.multiply(1,5));
+console.log(calculator.divide(1,3));
