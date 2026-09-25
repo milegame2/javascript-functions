194f7a03d6d7d22d7853541326d696ea56b60c37
Mile <milegame2@gmail.com>
2026-09-25 19:23:48 +0700

    Feat:Answer_Ex2

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,21 @@
 // Start coding here
+const add = function(a, b) {
+    return a + b;
+};
+
+const subtract = function(a, b) {
+    return a - b;
+};
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+const divide = function(a, b) {
+    return a / b;
+};
+
+console.log(add(1, 3));
+console.log(subtract(1, 4));
+console.log(multiply(1, 5));
+console.log(divide(1, 3));
